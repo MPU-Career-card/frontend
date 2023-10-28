fbc8a251859ee7ce480220c6146761ec903563f5
Mikhail Sergeev <sergeevm100@gmail.com>
2023-10-28 22:08:54 +0300

    created component

---
+++ src/shared/ui/tabs/tabs.tsx
@@ -0,0 +1,35 @@
+import React, {
+    FC, cloneElement, useState, ReactElement,
+} from 'react';
+
+import { cn } from 'shared/lib';
+
+import { TabsProps } from './types';
+
+import styles from './tabs.module.scss';
+
+export const Tabs: FC<TabsProps> = ({
+    justify = 'center',
+    value,
+    children,
+    onClick,
+}) => {
+    const childrenArray = React.Children
+        .toArray(children) as ReactElement[];
+
+    return (
+        <div className={cn(
+            styles.tabs,
+            styles[`justify-${justify}`],
+        )}
+        >
+            {childrenArray.map((tab) => cloneElement(
+                tab,
+                {
+                    active: tab.props.value === value,
+                    onClick: () => onClick(tab.props.value),
+                },
+            ))}
+        </div>
+    );
+};
---
+++ src/shared/ui/tabs/types.ts
@@ -0,0 +1,13 @@
+import React, { ComponentProps } from 'react';
+
+export type TabsProps = Omit<ComponentProps<'div'>, 'onClick'> & {
+  justify?:
+    | 'center'
+    | 'start'
+    | 'end'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  value: string;
+  onClick: React.Dispatch<React.SetStateAction<string>>;
+};
