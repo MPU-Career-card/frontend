f5ae97c933385ff50a2e8e47b6668b6054b6671e
AndrewwKovv <kovshov.K@yandex.ru>
2023-11-05 19:19:57 +0300

    delete element in home

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -4,22 +4,6 @@ import { Card, Scroll, Text } from 'shared/ui';
 const HomePage = () => (
     <Page>
         <Text>Главная</Text>
-        <Scroll justify="center">
-            <Card text="asdfsasdfasdf" />
-            <Card text="asdfsasdfasdfasdf" />
-            <Card text="asdfsasdfasdfasdf" />
-            <Card text="asdfasasdfasdfasdfs" />
-            <Card text="asdasdfasdfasdffs" />
-            <Card text="asdasdfasdfasdffs" />
-            <Card text="asdasdfasdffs" />
-            <Card text="asdfsasdfasdf" />
-            <Card text="asdfsasdfasdfasdf" />
-            <Card text="asdfsasdfasdfasdf" />
-            <Card text="asdfasasdfasdfasdfs" />
-            <Card text="asdasdfasdfasdffs" />
-            <Card text="asdasdfasdfasdffs" />
-            <Card text="asdasdfasdffs" />
-        </Scroll>
     </Page>
 );
 
