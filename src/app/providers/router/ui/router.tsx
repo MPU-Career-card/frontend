a6c3fa6e783cf627116281f5604874ebe991fbd3
emilkaraev <EKaraev@alfabank.ru>
2024-04-30 18:01:06 +0300

    fix: fixed package.json and router

--- src/app/providers/router/ui/router.tsx
+++ src/app/providers/router/ui/router.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import { Spinner } from 'shared/ui';
-
+import { Error } from 'pages';
 import { routerConfig } from '../config/router-config';
 
 export const Router = () => (
-    <Suspense fallback={<Spinner />}>
+    <Suspense fallback={<Error />}>
         <Routes>
             {Object.values(routerConfig).map(({ path, element }) => (
                 <Route
