f362b3007b492ecdb8eccb17d5d00d76a8bd3b64
Mikhail Sergeev <sergeevm100@gmail.com>
2024-10-06 21:39:58 +0300

    mobile changes

--- src/widgets/skills/ui/skills.tsx
+++ src/widgets/skills/ui/skills.tsx
@@ -1,5 +1,8 @@
-import { Text, Card } from 'shared/ui';
+import {
+    Text, Card, Flex, Scroll,
+} from 'shared/ui';
 import React, { FC } from 'react';
+import { cn, useWindowDimensions } from 'shared/lib';
 import { SkillsProps } from './types';
 import styles from './skills.module.scss';
 
@@ -7,20 +10,45 @@ export const Skills: FC<SkillsProps> = ({
     marginTop,
     marginBottom,
     skills,
-}) => (
-    <div className={styles.wrapper} style={{ marginTop, marginBottom }}>
-        <Text size="xl2" className={styles.title}>
-            Необходимые навыки
-        </Text>
-        <div className={styles.cardContainer}>
-            {skills.map(({ title, description, image }) => (
-                <Card
-                    key={title}
-                    title={title}
-                    text={description}
-                    image={image}
-                />
-            ))}
+}) => {
+    const sizeCategory = useWindowDimensions();
+
+    return (
+        <div style={{ marginTop, marginBottom }}>
+            <Text size="xl2" className={styles.title}>
+                Необходимые навыки
+            </Text>
+            <Flex
+                className={cn(styles.scroll)}
+                align="start"
+                gap={24}
+            >
+                {['xs', 's', 'm'].includes(sizeCategory) && (
+                    <Scroll gap={24}>
+                        {skills.map(({ title, description, image }) => (
+                            <Card
+                                width={300}
+                                key={title}
+                                title={title}
+                                text={description}
+                                image={image}
+                            />
+                        ))}
+                    </Scroll>
+                )}
+                {['xxl', 'xl', 'l'].includes(sizeCategory) && (
+                    <div className={styles.cardContainer}>
+                        {skills.map(({ title, description, image }) => (
+                            <Card
+                                key={title}
+                                title={title}
+                                text={description}
+                                image={image}
+                            />
+                        ))}
+                    </div>
+                )}
+            </Flex>
         </div>
-    </div>
-);
+    );
+};
