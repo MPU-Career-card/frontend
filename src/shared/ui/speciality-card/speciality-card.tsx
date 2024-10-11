6dcc0917ad58b9c3378a7d0fa6411de04765e1dd
Mikhail Sergeev <sergeevm100@gmail.com>
2024-10-11 15:41:33 +0300

    mobile button update

--- src/shared/ui/speciality-card/speciality-card.tsx
+++ src/shared/ui/speciality-card/speciality-card.tsx
@@ -7,6 +7,8 @@ import {
     Text,
 } from 'shared/ui';
 
+import { useWindowDimensions } from 'shared/lib';
+import { Arrow } from 'shared/icons';
 import { SpecialityCardProps } from './types';
 
 import styles from './speciality-card.module.scss';
@@ -17,23 +19,36 @@ export const SpecialityCard: FC<SpecialityCardProps> = ({
     name,
     className,
     isResponsiveHeight = false,
-}) => (
-    <CardLink
-        to={to}
-        color="white"
-        isResponsiveHeight={isResponsiveHeight}
-        className={className}
-        linkTag="a"
-    >
-        <Flex
-            className={styles.header}
-            direction="vertical"
-            align="start"
-            gap={26}
+}) => {
+    const sizeCategory = useWindowDimensions();
+    return (
+        <CardLink
+            to={to}
+            color="white"
+            isResponsiveHeight={isResponsiveHeight}
+            className={className}
+            linkTag="a"
         >
-            <Tag color="gray" size="s">{faculty}</Tag>
-            <Text size="l" weight="semi">{name}</Text>
-        </Flex>
-        <div />
-    </CardLink>
-);
+            <Flex
+                className={styles.header}
+                direction="vertical"
+                align="start"
+                gap={26}
+            >
+                <Tag color="gray" size="s">{faculty}</Tag>
+                <Text size="l" weight="semi">{name}</Text>
+            </Flex>
+            {['xs', 's', 'm'].includes(sizeCategory) && (
+                <Flex className={styles.mobileLink} justify="space-between">
+                    <Text color="white" size="m">
+                        Узнать подробнее
+                    </Text>
+                    <Arrow
+                        fill="white"
+                        style={{ width: '20px', height: '20px' }}
+                    />
+                </Flex>
+            )}
+        </CardLink>
+    );
+};
